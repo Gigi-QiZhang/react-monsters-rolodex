067a071fc95614a189cc97b19b984b5884fad10b
Gigi-QiZhang <dllgdxzq@gmail.com>
2020-03-03 23:09:07 -0500

    Updates

---
+++ precache-manifest.fcb67f6889bc91bc52050d51fe8d7f8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c2bdd3fa6738af53c34ea27f5c846f",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "05846319294ac5c14107",
+    "url": "/react-monsters-rolodex/static/css/main.8eff03d6.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/react-monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05846319294ac5c14107",
+    "url": "/react-monsters-rolodex/static/js/main.c8a65873.chunk.js"
+  },
+  {
+    "revision": "164e2d6773b8e1ceff39",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.453927e6.js"
+  }
+]);
